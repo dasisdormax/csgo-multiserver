32803204b06341d611518b1d1dd3daa8fc6042dd
Maximilian Wende <maximilian.wende@googlemail.com>
2016-04-01 14:32:24 +0200

    Added instance script for CS:GO server, further work on instance creation

--- scripts-740/instance.sh
+++ scripts-740/instance.sh
@@ -1,2 +1,13 @@
 # /bin/bash
-return 1
+
+# Directories that are fully copied, the instance owner can do whatever he wants there
+cp --reflink=auto -R "$INSTALL_DIR/csgo/addons" "$INSTANCE_DIR/csgo/addons"
+cp --reflink=auto -R "$INSTALL_DIR/csgo/cfg" "$INSTANCE_DIR/csgo/cfg"
+cp --reflink=auto -R "$INSTALL_DIR/csgo/models" "$INSTANCE_DIR/csgo/models"
+cp --reflink=auto -R "$INSTALL_DIR/csgo/sound" "$INSTANCE_DIR/csgo/sound"
+
+# Directories where the user can add own files in addition to the provided ones
+mkdir -p "$INSTANCE_DIR/csgo/maps"
+mkdir -p "$INSTANCE_DIR/csgo/maps/cfg"
+mkdir -p "$INSTANCE_DIR/csgo/maps/soundcache"
+mkdir -p "$INSTANCE_DIR/csgo/resource/overviews"
