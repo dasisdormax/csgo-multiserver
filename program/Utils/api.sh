fcbe6bcda4a596f934d178a056f477d2a82227c2
Maximilian Wende <maximilian.wende@gmail.com>
2016-11-17 09:12:56 +0100

    Added cl_api support, moved team stuff to functions

---
+++ program/Utils/api.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# (C) 2016 Maximilian Wende <maximilian.wende@gmail.com>
+#
+# This file is licensed under the Apache License 2.0. For more information,
+# see the LICENSE file or visit: http://www.apache.org/licenses/LICENSE-2.0
+
+
+
+
+CL_API_TOKEN="d05836cac806b383ea815d6416999908f93983ee"
+CL_API="https://campuslan.csn.tu-chemnitz.de/api/v1"
+
+cl_api_get () {
+	curl -L -X GET -H "Authorization: Token $CL_API_TOKEN" $CL_API/$1
+}
+
+
+# Gets teamname of team id $1
+cl_teamname () {
+	cl_api_get teams/$1/name
+}
+
+
+cl_team_steamids () {
+	cl_api_get teams/$1/steamids
+}
